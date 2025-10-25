4a561f6ead3dab0a3f18f2429c6eca4b1a871648
Enrique Ajin <enriquesaon@gmail.com>
2025-10-25 14:47:46 +0200

    Refactor Person entity

--- app/src/main/java/com/enridev/gitrebase/Person.kt
+++
@@ -1,8 +0,0 @@
-package com.enridev.gitrebase
-
-data class Person(
-    val name: String = "",
-    val age: Int = 0,
-    val email: String = "default@example.com",
-    val isEnabled: Boolean = true,
-)
---
+++ app/src/main/java/com/enridev/gitrebase/RefactoredPerson.kt
@@ -0,0 +1,8 @@
+package com.enridev.gitrebase
+
+data class RefactoredPerson(
+    val myCoolName: String = "",
+    val myCoolAge: Int = 0,
+    val myEmail: String = "default@example.com",
+    val isEnabled: Boolean = true,
+)
