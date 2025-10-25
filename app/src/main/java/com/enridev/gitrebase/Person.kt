e2d13921d8eba0d72784b1206215652c5d8cc0ca
Enrique Ajin <enriquesaon@gmail.com>
2025-10-25 14:45:35 +0200

    Create Person entity

---
+++ app/src/main/java/com/enridev/gitrebase/Person.kt
@@ -0,0 +1,8 @@
+package com.enridev.gitrebase
+
+data class Person(
+    val name: String = "",
+    val age: Int = 0,
+    val email: String = "default@example.com",
+    val isEnabled: Boolean = true,
+)
